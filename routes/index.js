caae4142e605a3a2515dd3c90bde4b3784aa9ee3
Marie <marie.roche32@orange.fr>
2025-09-29 18:50:42 +0200

    [ADD] api key + .env

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const fetch = require("node-fetch");
+
+const API_KEY = process.env.API_KEY;
+
+router.get('/movies', (req, res) => {
+ fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+   .then(response => response.json())
+   .then(data => {
+    console.log(data);
+    res.json({ movies: data.results })
+   });
 });
 
 module.exports = router;
